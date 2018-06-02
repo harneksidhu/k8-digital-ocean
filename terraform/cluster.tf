c08a87a6b7d46e8a965fbaf06d9d7402c474964b
Harnek Sidhu <harnek.sidhu@wishabi.com>
2018-06-02 18:55:05 -0400

    Adding terraform skeleton

---
+++ terraform/cluster.tf
@@ -0,0 +1,24 @@
+resource "digitalocean_droplet" "controller" {
+    count = 3
+    image = "ubuntu-18-04-x64"
+    name = "controller-${count.index}"
+    region = "tor1"
+    size = "1gb"
+    private_networking = true
+    ssh_keys = [
+      "${var.ssh_fingerprint}"
+    ]
+
+    connection {
+        user = "root"
+        type = "ssh"
+        private_key = "${file(var.pvt_key)}"
+        timeout = "2m"
+    }
+
+    provisioner "remote-exec" {
+    inline = [
+      "apt-get -y install python-minimal"
+    ]
+  }
+}
